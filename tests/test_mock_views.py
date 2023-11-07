8d1fbc1339d6c8469a33279cb78bc06fe0131c2d
Ubuntu <nickras10@gmail.com>
2023-11-07 16:34:13 +0000

    setting up basic mock tests

---
+++ tests/test_mock_views.py
@@ -0,0 +1,34 @@
+
+import pytest
+from unittest.mock import patch
+
+from learning.temp2 import main_function, create_player
+import learning.temp2 as temp2
+
+
+def test_main_function(monkeypatch):
+
+		def mockreturn():
+				return 100
+
+		monkeypatch.setattr(temp2, 'request', mockreturn)
+		expected_value = 100
+		assert main_function() == expected_value
+
+
+ 
+class MockResponse:
+ 
+    @staticmethod
+    def get_info():
+        return {"name": "test", "level" : 200}
+ 
+def test_create_player(monkeypatch):
+ 
+    def mock_get(*args, **kwargs):
+        return MockResponse() # returnthe dict with name and level
+ 
+    monkeypatch.setattr('learning.temp2.Player', mock_get)
+ 
+    expected_value = {"name": "test", "level" : 200}
+    assert create_player() == expected_value
